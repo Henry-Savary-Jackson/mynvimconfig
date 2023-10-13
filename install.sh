ae3f8339533bad21ea2dd7a0c1cddef9e7973408
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2023-10-13 11:57:08 +0200

    changed install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ ! -a "~/.local/share/nvim" ]; then
+	rm -rf ~/.local/share/nvim
+fi
+if [ ! -a "~/.local/state/nvim" ]; then
+	rm -rf ~/.local/state/nvim
+fi
+if [ ! -a "~/.config/nvim" ]; then
+	rm -rf ~/.config/nvim
+fi
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+echo "Now putting config... "
+
+#if [ ! -a "~/.config/nvim" ]; then
+#	rm -rf ~/.config/nvim
+#fi
+mv ~/.config/nvim $(pwd)/nvim-config/nvim  
+ln -sf $(pwd)/nvim-config/nvim ~/.config/
+
+#if [ ! -a "~/.local/state/nvim" ]; then
+#	rm -rf ~/.local/state/nvim
+#fi
+mv ~/.local/state/nvim $(pwd)/nvim-state/nvim 
+ln -sf $(pwd)/nvim-state/nvim ~/.local/state/
+
+#if [ ! -a "~/.local/share/nvim" ]; then
+#	rm -rf ~/.local/share/nvim
+#fi
+mv ~/.local/share/nvim $(pwd)/nvim-share/nvim 
+ln -sf $(pwd)/nvim-share/nvim ~/.local/share/
