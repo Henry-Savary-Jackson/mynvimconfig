79db389e1c1fb4cca5d4c4bc9955030e503778a6
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2023-10-21 21:14:57 +0200

    kept only necessary config dir

--- install.sh
+++ install.sh
@@ -17,15 +17,3 @@ if [ ! -d "$HOME/.config/nvim" ]; then
 	rm -rf ~/.config/nvim
 fi
 ln -sf $(pwd)/nvim-config/nvim ~/.config/
-
-#if [ ! -a "~/.local/state/nvim" ]; then
-#	rm -rf ~/.local/state/nvim
-#fi
-mv ~/.local/state/nvim $(pwd)/nvim-state/nvim 
-ln -sf $(pwd)/nvim-state/nvim ~/.local/state/
-
-#if [ ! -a "~/.local/share/nvim" ]; then
-#	rm -rf ~/.local/share/nvim
-#fi
-mv ~/.local/share/nvim $(pwd)/nvim-share/nvim 
-ln -sf $(pwd)/nvim-share/nvim ~/.local/share/
