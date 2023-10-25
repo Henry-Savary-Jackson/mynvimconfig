4cf7b5b18759abff96168906159c57a4b8433b87
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2023-10-25 23:42:44 +0200

    coc-pyright works

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,7 @@ if not vim.loop.fs_stat(lazypath) then
   require("core.bootstrap").gen_chadrc_template()
   require("core.bootstrap").lazy(lazypath)
 end
+vim.wo.relativenumber = true
 
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
