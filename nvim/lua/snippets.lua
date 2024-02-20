9c02b777d00b689d2900d4f7c259849a2b83954a
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2024-02-20 03:13:55 +0200

    FINALLY STOPPED BEING LAZY AND ADDED LUASNIP LATEX SNIPPETS

---
+++ nvim/lua/snippets.lua
@@ -0,0 +1,69 @@
+local ls = require "luasnip"
+
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require "luasnip.util.types"
+local conds = require "luasnip.extras.expand_conditions"
+
+ls.add_snippets("tex", {
+  s("cos", { t "\\cos(", i(1), t ")" }),
+  s("sin", { t "\\sin(", i(1), t ")" }),
+  s("tan", { t "\\tan(", i(1), t ")" }),
+  s("cot", { t "\\cot(", i(1), t ")" }),
+  s("sec", { t "\\sec(", i(1), t ")" }),
+  s("csc", { t "\\csc(", i(1), t ")" }),
+
+  s("equiv", { t "\\equiv" }),
+  s("<", { t "\\lt" }),
+  s(">", { t "\\gt" }),
+  s(">=", { t "\\ge" }),
+  s("<=", { t "\\le" }),
+
+  s("inf", { t "\\infty" }),
+  s("Real", { t "\\Re" }),
+  s("in", { t "\\in" }),
+  s("union", { t "\\cup" }),
+  s("inters", { t "\\cap" }),
+  s("sset", { t "\\subset" }),
+  s("psset", { t "\\subseteq" }),
+  s("nil", { t "\\emptyset" }),
+
+  s("lim", { t "\\lim_{", i(1), t "\\to", i(2), t "}" }),
+  s("liminf", { t "\\lim_{", i(1), t "\\to \\infty", t "}" }),
+  s("int", { t "\\int_{", i(1), t "}^{", i(2), t "}" }),
+  s("iint", { t "\\int" }),
+  s("sum", { t "\\sum_{", i(1), t "}^{", i(2), t "}" }),
+  s("sum", { t "\\sum_{", i(1), t "}^{\\infty}" }),
+  s("dx", { t "\\frac{", i(2), t "}{dx}" }),
+
+  s("^", { t "^{", i(1), t "}" }),
+  s("root", { t "sqrt[", i(1), t "]{", i(2), t "}" }),
+  s("sqrt", { t "sqrt{", i(1), t "}" }),
+  s("%", { t "\\mod" }),
+  s("xx", { t "\\times" }),
+  s("frac", { t "\\frac{", i(1), t "}{", i(2), t "}" }),
+
+  s("dm", { t { "\\[", " " }, i(1), t { " ", "\\]" } }),
+  s("im", { t "$", i(1), t "$" }),
+
+  s("cases", { t { "\\begin{cases}", " " }, i(1), t "&", i(2), t { " ", "\\end{cases}" } }),
+
+  s("vec", { t "\\vec{", i(1), t "}" }),
+
+  s("_", { t "_{", i(1), t "}" }),
+  s("|", { t "|", i(1), t "|" }),
+})
